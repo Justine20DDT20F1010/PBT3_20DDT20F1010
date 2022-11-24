de43e75290adec432dbb8690e4c87927259dcdff
justineDDT5A <justinemajau1702@gmail.com>
2022-11-24 22:46:22 +0800

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtweight = New System.Windows.Forms.TextBox()
+        Me.btnsubmit = New System.Windows.Forms.Button()
+        Me.txtheight = New System.Windows.Forms.TextBox()
+        Me.lbdisplay = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Btn_Clear = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ToolStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtweight
+        '
+        Me.txtweight.Location = New System.Drawing.Point(98, 264)
+        Me.txtweight.Multiline = True
+        Me.txtweight.Name = "txtweight"
+        Me.txtweight.Size = New System.Drawing.Size(149, 31)
+        Me.txtweight.TabIndex = 0
+        '
+        'btnsubmit
+        '
+        Me.btnsubmit.BackColor = System.Drawing.Color.GreenYellow
+        Me.btnsubmit.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnsubmit.Location = New System.Drawing.Point(165, 380)
+        Me.btnsubmit.Name = "btnsubmit"
+        Me.btnsubmit.Size = New System.Drawing.Size(154, 35)
+        Me.btnsubmit.TabIndex = 1
+        Me.btnsubmit.Text = "Submit"
+        Me.btnsubmit.UseVisualStyleBackColor = False
+        '
+        'txtheight
+        '
+        Me.txtheight.Location = New System.Drawing.Point(98, 155)
+        Me.txtheight.Multiline = True
+        Me.txtheight.Name = "txtheight"
+        Me.txtheight.Size = New System.Drawing.Size(149, 27)
+        Me.txtheight.TabIndex = 3
+        '
+        'lbdisplay
+        '
+        Me.lbdisplay.FormattingEnabled = True
+        Me.lbdisplay.Location = New System.Drawing.Point(275, 124)
+        Me.lbdisplay.Name = "lbdisplay"
+        Me.lbdisplay.Size = New System.Drawing.Size(322, 212)
+        Me.lbdisplay.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Snow
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(135, 134)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 18)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Height (m)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Snow
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(127, 243)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(95, 18)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Weight (kg)"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.BackColor = System.Drawing.Color.SteelBlue
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripButton2, Me.ToolStripSeparator1, Me.ToolStripButton3})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(695, 25)
+        Me.ToolStrip1.TabIndex = 30
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), System.Drawing.Image)
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton2.Text = "ToolStripButton2"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton3
+        '
+        Me.ToolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton3.Image = CType(resources.GetObject("ToolStripButton3.Image"), System.Drawing.Image)
+        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton3.Name = "ToolStripButton3"
+        Me.ToolStripButton3.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton3.Text = "ToolStripButton3"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.PeachPuff
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(46, 72)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(605, 288)
+        Me.PictureBox1.TabIndex = 31
+        Me.PictureBox1.TabStop = False
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(281, 40)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(137, 29)
+        Me.Label3.TabIndex = 32
+        Me.Label3.Text = "Lab Result"
+        '
+        'Btn_Clear
+        '
+        Me.Btn_Clear.BackColor = System.Drawing.Color.Crimson
+        Me.Btn_Clear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn_Clear.Location = New System.Drawing.Point(350, 379)
+        Me.Btn_Clear.Name = "Btn_Clear"
+        Me.Btn_Clear.Size = New System.Drawing.Size(142, 35)
+        Me.Btn_Clear.TabIndex = 33
+        Me.Btn_Clear.Text = "Reset"
+        Me.Btn_Clear.UseVisualStyleBackColor = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(356, 97)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(187, 24)
+        Me.Label4.TabIndex = 34
+        Me.Label4.Text = "Health Point Records"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(695, 428)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Btn_Clear)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbdisplay)
+        Me.Controls.Add(Me.txtheight)
+        Me.Controls.Add(Me.btnsubmit)
+        Me.Controls.Add(Me.txtweight)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.RightToLeftLayout = True
+        Me.Text = "Caspian "
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtweight As TextBox
+    Friend WithEvents btnsubmit As Button
+    Friend WithEvents txtheight As TextBox
+    Friend WithEvents lbdisplay As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripButton3 As ToolStripButton
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PrintForm1 As PowerPacks.Printing.PrintForm
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Btn_Clear As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,76 @@
+﻿'Name : Justine Nanggai
+'No matrik : 20DDT20F1010
+'Class : DDT5A
+'PBT : 3
+Public Class Form1
+    Private Sub btnsubmit_Click(sender As Object, e As EventArgs) Handles btnsubmit.Click
+        'declare array for arrayheight, arrayweight and array bmi
+        Dim I As Integer
+        Dim ArrayHeight(10) As Integer
+        Dim ArrayWeight(10) As Integer
+        Dim health(10) As String
+        Dim ArrayBMU(10) As Double
+        If I < ArrayHeight.Length Then
+            If txtheight.Text.Trim.Length > 0 Then
+                ArrayHeight(I) = txtheight.Text
+                ArrayWeight(I) = txtheight.Text
+
+                'BMI = Weight/height^2(m)
+                '<18.4 underweight
+                '18.5-24.9 normal
+                '25-39.9 overweight
+                '>40 obese
+                'formula for calculate the bmi
+                ArrayBMU(I) = ArrayWeight(I) / (ArrayHeight(I) * ArrayHeight(I))
+
+                'if the bmi more than 40 or equal to the listbox will display underweight
+                If ArrayBMU(I) >= 40 Then
+                    health(I) = "obese"
+                    'if the bmi more than 25 or equal to the  listbox will display overweight
+                ElseIf ArrayBMU(I) >= 25 Then
+                    health(I) = "overweight"
+                    'if the bmi more than 18.5 or equal to the listbox
+                ElseIf ArrayBMU(I) >= 18.5 Then
+                    health(I) = "normal"
+                Else
+                    'if the bmi less than 25
+                    health(I) = "underweight"
+                End If
+                'display in listbox
+                lbdisplay.Items.Add(ArrayHeight(I) & "  " & ArrayWeight(I) & "  " & ArrayBMU(I) & "  " & health(I))
+                I = I + 1
+            Else
+                'if the user do not enter the data
+                MsgBox("Please Enter Data")
+            End If
+        Else
+            'if the array is full
+            MsgBox("The array is Full")
+        End If
+
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        'open dialog 
+        OpenFileDialog1.ShowDialog()
+
+    End Sub
+
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+        'save dialog 
+        SaveFileDialog1.ShowDialog()
+
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+        'print form
+        PrintForm1.PrintAction = Printing.PrintAction.PrintToPreview
+        PrintForm1.Print()
+    End Sub
+
+    Private Sub Btn_Clear_Click(sender As Object, e As EventArgs) Handles Btn_Clear.Click
+        'clear the textbox
+        txtheight.Clear()
+        txtweight.Clear()
+    End Sub
+End Class
